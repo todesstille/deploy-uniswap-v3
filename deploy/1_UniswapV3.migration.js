8303ab6792794505a044484edc1b4f4e2f9271cf
Dmitry Redkin <tanatonaut@gmail.com>
2024-05-16 22:11:03 +0300

    position managr and swap

--- deploy/1_UniswapV3.migration.js
+++ deploy/1_UniswapV3.migration.js
@@ -3,7 +3,7 @@ const { Deployer, Reporter } =require("@solarity/hardhat-migrate");
 
 // Comment toolbox and uncomment migrate and waffle inside hardhat.config.js
 module.exports = async (deployer) => {
-  const Token = await hre.ethers.getContractFactory("MockToken");
-  
-  const token = await deployer.deploy(Token);
+  const Factory = await hre.ethers.getContractFactory("UniswapV3Factory");
+
+  await deployer.deploy(Factory);
 };
--- test/UniswapV3.test.js
+++ test/UniswapV3.test.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { BigNumber } = require("ethers");
@@ -14,16 +15,29 @@ describe("Test template", function () {
     const MockToken = await ethers.getContractFactory("MockToken");
     const WETH9 = await ethers.getContractFactory("WETH9");
     const UniswapV3Factory = await ethers.getContractFactory("UniswapV3Factory");
-    // const UniswapV2Router01 = await ethers.getContractFactory("UniswapV2Router01");
-    // const UniswapV2Router02 = await ethers.getContractFactory("UniswapV2Router02");
+    const SwapRouter = await ethers.getContractFactory("SwapRouter");
+    const NFTDescriptor = await ethers.getContractFactory("NFTDescriptor");
+    const nftDescriptor = await NFTDescriptor.deploy();
+    const NonfungibleTokenPositionDescriptor = await ethers.getContractFactory(
+      "NonfungibleTokenPositionDescriptor", {
+        libraries: {
+          NFTDescriptor: nftDescriptor.address,
+        },
+      });
+    const NonfungiblePositionManager = await ethers.getContractFactory("NonfungiblePositionManager");
     
     factoryV3 = await UniswapV3Factory.deploy();
     token0 = await MockToken.deploy("Mock Token 0", "MCK0", "1000000000000000000000000000");
     token1 = await MockToken.deploy("Mock Token 1", "MCK1", "1000000000000000000000000000");
 
     weth = await WETH9.deploy();
-    // router1 = await UniswapV2Router01.deploy(factoryV2.address, weth.address);
-    // router2 = await UniswapV2Router02.deploy(factoryV2.address, weth.address);
+
+    const ncl = process.env.NATIVE_CURRENCY_LABEL;
+    tokenDescriptor = await NonfungibleTokenPositionDescriptor.deploy(weth.address, ethers.utils.formatBytes32String(ncl));
+
+    positionManager = await NonfungiblePositionManager.deploy(factoryV3.address, weth.address, tokenDescriptor.address);
+
+    swapRouter = await SwapRouter.deploy(factoryV3.address, weth.address);
   });
 
   describe("Tokens", function () {
@@ -42,17 +56,40 @@ describe("Test template", function () {
       // 1:1 ratio
       await pool.initialize("0x01000000000000000000000000");
 
-      // expect((await pair.getReserves())[0]).to.equal(0);
-      // expect((await pair.getReserves())[1]).to.equal(0);
+      const tickSpacing = await pool.tickSpacing();
+      const maxTick = 887272 - (887272 % tickSpacing);
 
-      // await token0.transfer(pair.address, "10000000000000000000");
-      // await token1.transfer(pair.address, "10000000000000000000");
+      const amount = ethers.utils.parseEther("1");
 
-      // await pair.mint(admin.address);
-      // expect(await pair.balanceOf(admin.address)).to.equal("9999999999999999000");
+      await token0.approve(positionManager.address, amount);
+      await token1.approve(positionManager.address, amount);
 
-      // await token0.approve(router2.address, 100000);
-      // await router2.swapExactTokensForTokens(100000, 0, [token0.address, token1.address], admin.address, 9999999999)
+      await positionManager.mint([
+        token0.address,
+        token1.address,
+        500,
+        -maxTick,
+        maxTick,
+        amount,
+        amount,
+        0,
+        0,
+        admin.address,
+        9999999999
+      ]);
+
+      await token0.approve(swapRouter.address, 1100000);
+
+      await swapRouter.exactOutputSingle([
+        token0.address,
+        token1.address,
+        500,
+        alice.address,
+        9999999999,
+        1000000,
+        1100000,
+        0
+      ]);
     });
   });
 });
